dee733c8e28adb4f0984d00972d95f66e517b804
Rebeca Chiñas <chi.san.rebeca@gmail.com>
2019-12-12 21:45:05 -0600

    Trying  to deploy

---
+++ frontend/build/precache-manifest.4bea8940db6d55fa5cd52910a06d6d7c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52abff6695146bf4148ddbb819461999",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56ae6a56ba78c8f36f1b",
+    "url": "/static/css/main.694714ff.chunk.css"
+  },
+  {
+    "revision": "5d884676b64ca81e6b52",
+    "url": "/static/js/2.21bae06f.chunk.js"
+  },
+  {
+    "revision": "56ae6a56ba78c8f36f1b",
+    "url": "/static/js/main.e5e0f084.chunk.js"
+  },
+  {
+    "revision": "ed4750f41f4543566353",
+    "url": "/static/js/runtime-main.fbe98da3.js"
+  }
+]);
